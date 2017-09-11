f9e4f3a57d7d0577dce89f0cfc4aeacd10d16ef5
Dmitry <mr.remv@mail.ru>
2017-09-11 17:01:10 +0300

    Update MainActivity.java

--- app/src/main/java/ru/currencyconverter/activity/view/MainActivity.java
+++ app/src/main/java/ru/currencyconverter/activity/view/MainActivity.java
@@ -1,6 +1,5 @@
 package ru.currencyconverter.activity.view;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,8 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
